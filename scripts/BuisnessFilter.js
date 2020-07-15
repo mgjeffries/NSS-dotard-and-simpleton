5fcc93b957418bf9478bd7afb9104f14469575a0
mgjeffries <gib.jeffries@gmail.com>
2020-07-15 16:16:52 -0500

    filter manufacturing businesses

--- scripts/BuisnessFilter.js
+++ scripts/BuisnessFilter.js
@@ -1,6 +1,17 @@
-export const filterBusinesses = ( allBuisnesses ) => {
+export const filterBusinessNY = ( allBuisnesses ) => {
   return allBuisnesses.filter(buisnessObj => {
     if (buisnessObj.addressStateCode === "NY") {
+      return true
+    }
+    else {
+      return false
+    } 
+  })
+}
+
+export const filterBusinessManufacturing = ( allBuisnesses ) => {
+  return allBuisnesses.filter(buisnessObj => {
+    if (buisnessObj.companyIndustry === "Manufacturing") {
       return true
     }
     else {
--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,14 +1,19 @@
 import { businessHTMLCreator } from './Business.js'
 import { useBusinesses } from './BusinessProvider.js'
-import { filterBusinesses } from './BuisnessFilter.js'
+import { filterBusinessNY } from './BuisnessFilter.js'
+import { filterBusinessManufacturing } from './BuisnessFilter.js'
 
 export const listBusinesses = () => { 
   const contentTarget = document.querySelector(".business-list")
 
   const allBuisnessesArray = useBusinesses()
-  const filteredBusinessArray = filterBusinesses(allBuisnessesArray)
+  const nyBusinesses = filterBusinessNY(allBuisnessesArray)
+  const manufacturingBuisnesses = filterBusinessManufacturing(allBuisnessesArray)
 
-  filteredBusinessArray.forEach(business => {
+  nyBusinesses.forEach(business => {
+    contentTarget.innerHTML += businessHTMLCreator(business)
+  });
+  manufacturingBuisnesses.forEach(business => {
     contentTarget.innerHTML += businessHTMLCreator(business)
   });
 }
