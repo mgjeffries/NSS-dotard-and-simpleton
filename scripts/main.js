6fe25138c513492ec772b7a8d1139f852598173f
mgjeffries <gib.jeffries@gmail.com>
2020-07-15 15:21:32 -0500

    move buisness listing process to BuisnessList

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,3 @@
-import { useBusinesses } from './BusinessProvider.js'
-import { businessHTMLCreator } from './Business.js';
+import { listBusinesses } from './BusinessList.js'
 
-const contentTarget = document.querySelector(".business-list")
-
-const allBuisnessesArray = useBusinesses()
-
-allBuisnessesArray.forEach(business => {
-  contentTarget.innerHTML += businessHTMLCreator(business)
-});
+listBusinesses()
